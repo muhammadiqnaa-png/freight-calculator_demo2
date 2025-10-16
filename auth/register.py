aa07d7f8c6e6a13791800b32c1808a808e2c388c
muhammadiqnaa-png <muhammadiqnaa@gmail.com>
2025-10-16 22:04:10 +0700

    Update register.py

--- auth/register.py
+++ auth/register.py
@@ -1,24 +1,24 @@
 import streamlit as st
-import pyrebase
-from .firebase_config import firebase_config
+from auth.firebase_config import auth
+from requests.exceptions import HTTPError
 
-firebase = pyrebase.initialize_app(firebase_config)
-auth = firebase.auth()
-
-def register_page():
-    st.title("📝 Register")
+def register():
+    st.title("🧾 Register Freight Calculator")
 
     email = st.text_input("Email")
     password = st.text_input("Password", type="password")
 
-    if st.button("Register"):
-        if email and password:
-            try:
-                user = auth.create_user_with_email_and_password(email, password)
-                st.success("Register sukses! Silakan login")
-                st.session_state.page = "login"
-                st.experimental_rerun()
-            except Exception as e:
-                st.error(f"Register gagal: {e}")
-        else:
-            st.warning("Isi email dan password")
+    if st.button("Daftar"):
+        try:
+            user = auth.create_user_with_email_and_password(email, password)
+            st.success("Akun berhasil dibuat ✅ Silakan login.")
+            st.switch_page("auth/login.py")
+        except HTTPError as e:
+            error_json = e.response.json()
+            error_message = error_json["error"]["message"]
+            st.error(f"Gagal registrasi: {error_message}")
+
+    st.write("---")
+    st.write("Sudah punya akun?")
+    if st.button("Login disini"):
+        st.switch_page("auth/login.py")
