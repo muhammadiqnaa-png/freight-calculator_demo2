2e41ffbc22c683d00443ebdf757c16e1f534c5e0
muhammadiqnaa-png <muhammadiqnaa@gmail.com>
2025-10-16 20:45:34 +0700

    Update freight_calculator.py

--- pages/freight_calculator.py
+++ pages/freight_calculator.py
@@ -19,4 +19,4 @@ def freight_page():
         for key in list(st.session_state.keys()):
             del st.session_state[key]
         st.session_state.page = "login"
-        st.experimental_rerun()
+        st.rerun()
