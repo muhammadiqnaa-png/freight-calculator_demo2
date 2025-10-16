17effa69b7bebbf474ab18f12b969be9b8dd1a95
muhammadiqnaa-png <muhammadiqnaa@gmail.com>
2025-10-16 20:31:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,18 @@ import streamlit as st
 
 st.set_page_config(page_title="Freight Calculator Barge", page_icon="⚓", layout="wide")
 
-# Kalau user belum login, arahkan ke halaman login
-if "user" not in st.session_state:
-    st.switch_page("auth/login.py")
-else:
-    st.switch_page("pages/freight_calculator.py")
+# --- Redirect sederhana tanpa switch_page() ---
+if "page" not in st.session_state:
+    st.session_state.page = "login"
+
+if st.session_state.page == "login":
+    from auth.login import login_page
+    login_page()
+
+elif st.session_state.page == "register":
+    from auth.register import register_page
+    register_page()
+
+elif st.session_state.page == "freight":
+    from pages.freight_calculator import freight_page
+    freight_page()
