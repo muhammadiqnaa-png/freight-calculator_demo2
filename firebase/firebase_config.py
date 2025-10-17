eeb7da123dcd00a482d1027b16df5e835a67742e
muhammadiqnaa-png <muhammadiqnaa@gmail.com>
2025-10-18 02:51:46 +0700

    Update firebase_config.py

--- firebase/firebase_config.py
+++ firebase/firebase_config.py
@@ -1,6 +1,12 @@
-import firebase_admin
-from firebase_admin import credentials, firestore, auth
+import pyrebase
 
-cred = credentials.Certificate("path/to/serviceAccountKey.json")  # Ambil dari Firebase
-firebase_admin.initialize_app(cred)
-db = firestore.client()
+config = {
+  "apiKey": "AIzaSyDRxbw6-kJQsXXXr0vpnlDqhaUWKOjmQIU",
+  "authDomain": "freight-demo2.firebaseapp.com",
+  "databaseURL": "https://freight-demo2.firebaseio.com",
+  "storageBucket": "freight-demo2.firebasestorage.app"
+}
+
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+db = firebase.database()
