0015bd2254439da67b87c2c29e82960b35347d9a
muhammadiqnaa-png <muhammadiqnaa@gmail.com>
2025-10-18 01:43:17 +0700

    Update login.py

--- auth/login.py
+++ auth/login.py
@@ -1,15 +1,1 @@
-import streamlit as st
-from auth.firebase_config import sign_in
 
-def login_page():
-    st.title("🚛 Freight Calculator Login")
-
-    email = st.text_input("Email")
-    password = st.text_input("Password", type="password")
-
-    if st.button("Login"):
-        user = sign_in(email, password)
-        if user:
-            st.session_state["user"] = user.email
-            st.success(f"Selamat datang, {user.email}!")
-            st.switch_page("pages/freight_calculator.py")
