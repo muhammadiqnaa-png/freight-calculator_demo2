639989c170a0bd61a9e8de0e84ea1faced44c36f
muhammadiqnaa-png <muhammadiqnaa@gmail.com>
2025-10-18 02:43:05 +0700

    Update firebase_config.py

--- firebase/firebase_config.py
+++ firebase/firebase_config.py
@@ -1,6 +1,6 @@
 import firebase_admin
 from firebase_admin import credentials, firestore, auth
 
-cred = credentials.Certificate("serviceAccountKey.json")  # Ambil dari Firebase
+cred = credentials.Certificate("path/to/serviceAccountKey.json")  # Ambil dari Firebase
 firebase_admin.initialize_app(cred)
 db = firestore.client()
