4532549889363ea6b726d0618c4c5b13e3f07148
muhammadiqnaa-png <muhammadiqnaa@gmail.com>
2025-10-18 02:26:26 +0700

    Delete utils directory

--- utils/footer.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-def footer(user):
-    st.markdown("---")
-    st.markdown(
-        f"<p style='text-align:center; font-size:12px;'>Generated Freight Calculator by <b>{user}</b></p>",
-        unsafe_allow_html=True
-    )
