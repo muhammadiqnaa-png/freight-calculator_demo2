54b4216f79bc8f36d0c6e2ad679b414d85b85bc4
muhammadiqnaa-png <muhammadiqnaa@gmail.com>
2025-10-18 00:32:23 +0700

    Update firebase_config.py

--- auth/firebase_config.py
+++ auth/firebase_config.py
@@ -1,12 +1,12 @@
 import pyrebase4 as pyrebase
 
 firebaseConfig = {
-    "apiKey": "AIzaSyDRxbw6-kJQsXXXr0vpnlDqhaUWKOjmQIU",
+    "apiKey": "AIzaSyDRxbw6-kJQsXXXr0vpnlDqhaUWKOjmQIU", 
     "authDomain": "freight-demo2.firebaseapp.com",
     "projectId": "freight-demo2",
-    "storageBucket": "freight-demo2.firebasestorage.app",
+    "storageBucket": "freight-demo2.appspot.com",
     "messagingSenderId": "199645170835",
-    "appId": "1:199645170835:web:efa8ff8d5b85416eb71166",
+    "appId": "1:199645170835:web:efa8ff8d5b85416eb71166", 
     "databaseURL": ""
 }
 
