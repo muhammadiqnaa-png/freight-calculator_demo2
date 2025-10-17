4e6fa4cfda750a46e46946e744bf7708898823e1
muhammadiqnaa-png <muhammadiqnaa@gmail.com>
2025-10-18 01:18:19 +0700

    Update firebase_config.py

--- auth/firebase_config.py
+++ auth/firebase_config.py
@@ -2,16 +2,18 @@ import streamlit as st
 import firebase_admin
 from firebase_admin import credentials, auth
 
-# Ambil credential dari Streamlit secrets
+# Load konfigurasi dari secrets.toml
+firebase_config = st.secrets["firebase"]
+
+# Inisialisasi Firebase
 if not firebase_admin._apps:
-    cred = credentials.Certificate(st.secrets["firebase"])
+    cred = credentials.Certificate(firebase_config)
     firebase_admin.initialize_app(cred)
 
-# Fungsi login manual pakai email & password (opsional)
 def sign_in(email, password):
     try:
         user = auth.get_user_by_email(email)
         return user
     except Exception as e:
-        st.error(f"Gagal login: {e}")
+        st.error(f"Login gagal: {e}")
         return None
