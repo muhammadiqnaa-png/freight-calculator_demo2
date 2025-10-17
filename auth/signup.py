1c22ae114ba03eac2b5c4d8fd37ba5ea58cc4c5a
muhammadiqnaa-png <muhammadiqnaa@gmail.com>
2025-10-18 01:42:59 +0700

    Update signup.py

--- auth/signup.py
+++ auth/signup.py
@@ -1,19 +1,1 @@
-import streamlit as st
-from auth.firebase_config import auth
 
-def register_page():
-    st.title("📝 Daftar Akun Baru")
-
-    with st.form("register_form"):
-        email = st.text_input("Email")
-        password = st.text_input("Password", type="password")
-        submit = st.form_submit_button("Daftar")
-
-    if submit:
-        try:
-            user = auth.create_user_with_email_and_password(email, password)
-            st.success("Akun berhasil dibuat! Silakan login.")
-            st.session_state.show_register = False
-            st.rerun()
-        except Exception as e:
-            st.error("Gagal membuat akun. Pastikan password minimal 6 karakter.")
