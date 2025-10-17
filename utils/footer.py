782381c37c58df7e2517dd089e098b2ac2440a81
muhammadiqnaa-png <muhammadiqnaa@gmail.com>
2025-10-18 00:24:27 +0700

    Update footer.py

--- utils/footer.py
+++ utils/footer.py
@@ -1,17 +1,8 @@
 import streamlit as st
 
-def show_footer(user_email=None):
+def footer(user):
     st.markdown("---")
-    if user_email:
-        st.markdown(
-            f"<p style='text-align:center;font-size:13px;color:gray;'>"
-            f"Generated Freight Calculator by <b>{user_email}</b>"
-            f"</p>",
-            unsafe_allow_html=True,
-        )
-    else:
-        st.markdown(
-            "<p style='text-align:center;font-size:13px;color:gray;'>"
-            "Generated Freight Calculator by Iqna</p>",
-            unsafe_allow_html=True,
-        )
+    st.markdown(
+        f"<p style='text-align:center; font-size:12px;'>Generated Freight Calculator by <b>{user}</b></p>",
+        unsafe_allow_html=True
+    )
