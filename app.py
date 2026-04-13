158e6414fa63132ee37f771f608b5b4344474497
muhammadiqnaa-png <muhammadiqnaa@gmail.com>
2026-04-13 11:05:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -116,7 +116,7 @@ if "distance_data" not in st.session_state:
         {"pol": "TIA, Bunati", "pod": "Ciwandan", "distance": 610},
         {"pol": "MBL, Samarinda", "pod": "Southport, Klang Malaysia", "distance": 1435},
         {"pol": "Kideco, Grogot", "pod": "Tanjung Kasam, Batam", "distance": 995},
-        {"pol": "Lampung, Muara Teladas", "pod": "Marunda", "distance": 125},
+        {"pol": "Lampung, Muara Teladas", "pod": "MARUNDA", "distance": 125},
         
     ]
 
