027331fed5589a1c4573184d8d5ad2ea115c5e7f
muhammadiqnaa-png <muhammadiqnaa@gmail.com>
2026-04-14 13:48:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -623,17 +623,18 @@ if st.button("🚀 Calculate Freight", use_container_width=True):
         st.subheader("📊 Summary")
 
         col1, col2, col3 = st.columns(3)
-    
-        with col1:
-            st.metric("🚢 Voyage Days", f"{total_voyage_days:.1f}")
 
-        with col2:
+        with col1:
+            st.metric("🚢 Voyage Days", f"{total_voyage_days:.2f}")
             st.metric("⛽ Fuel Cost", f"Rp {cost_fuel:,.0f}")
 
-        with col3:
-            st.metric("💰 Total Cost", f"Rp {total_cost:,.0f}")
+        with col2:
+            st.metric("⏱ Sailing Time (Hr)", f"{sailing_time:.2f}")
+            st.metric("💧 Freshwater Cost", f"Rp {cost_fw:,.0f}")
 
-        st.markdown('</div>', unsafe_allow_html=True)
+        with col3:
+            st.metric("🛢 Fuel Consumption", f"{total_consumption_fuel:,.0f} Ltr")
+            st.metric("🚰 FW Consumption", f"{total_consumption_fw:,.0f} Ton")
 
         if mode == "Owner":
             st.markdown("### 🏗️ Owner Costs Summary")
